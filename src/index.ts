52c0f2e0f974b101f4cea65c4ceef7ef0ee09529
denamolio <71786326+dacadla@users.noreply.github.com>
2023-10-29 11:09:19 +0100

    make fullPath a let variable

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,15 @@ if (Bun.argv[2] == undefined) {
     process.exit(1);
 }
 
+let fullPath: string;
 // get the full path of the file, if errors out, exit
 try {
-    var fullPath = Bun.resolveSync(Bun.argv[2], process.cwd());
+    fullPath = Bun.resolveSync(Bun.argv[2], process.cwd());
 } catch (_) {
     console.log("File not found");
     process.exit(1);
 }
 
 // read the file
-let code = await Bun.file(fullPath).text();
+let code: string = await Bun.file(fullPath).text();
 console.log(parser.parse(code));
